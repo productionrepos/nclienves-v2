c170f91635252e57aff45a927eeb842399d5c8a4
Eduardo <edu.aguirrebena@gmail.com>
2023-03-10 14:54:29 -0300

    change echos pedido_credito

--- ws/pedidos/pedido_credito.php
+++ ws/pedidos/pedido_credito.php
@@ -6,17 +6,17 @@ $id_pedido = filter_input(INPUT_POST, "id_pedido", FILTER_SANITIZE_NUMBER_INT);
 $token = filter_input(INPUT_POST, "token", FILTER_SANITIZE_STRING);
 
 if(empty($id_pedido) && !is_numeric($id_pedido)):
-    print_r(json_encode(array("success" => 0, "message" => "Algo anda muy mal")));
+    echo (json_encode(array("success" => 0, "message" => "Algo anda muy mal")));
     exit();
 endif;
 
 if(empty($token) || strlen($token)!=32):
-    print_r(json_encode(array("success" => 0, "message" => "Se requiere el token, intente recargando la página")));
+    echo (json_encode(array("success" => 0, "message" => "Se requiere el token, intente recargando la página")));
     exit();
 endif;
 
 if($token!=md5($id_pedido.$id_cliente."pedido_credito#")) {
-    print_r(json_encode(array("success" => 0, "message" => "Token inválido, intente recargando la página")));
+    echo (json_encode(array("success" => 0, "message" => "Token inválido, intente recargando la página")));
     exit();
 }
 
@@ -24,18 +24,18 @@ include('../bd/dbconn.php');
 $conexion = new bd();
 $conexion->conectar();
 
-$query = 'SELECT * FROM pedido
-		INNER JOIN datos_contacto ON (pedido.id_cliente=datos_contacto.id_cliente)
-		INNER JOIN datos_comerciales ON (pedido.id_cliente=datos_comerciales.id_cliente)
-		INNER JOIN bodega ON (pedido.id_bodega=bodega.id_bodega)
-		INNER JOIN comuna ON (bodega.id_comuna=comuna.id_comuna)
-		INNER JOIN provincia ON (comuna.id_provincia=provincia.id_provincia)
-		INNER JOIN region ON (provincia.id_region=region.id_region) 
-		INNER JOIN cliente ON (pedido.id_cliente=cliente.id_cliente)
-		AND pedido.id_pedido= '.$id_pedido;
+// $query = 'SELECT * FROM pedido
+// 		INNER JOIN datos_contacto ON (pedido.id_cliente=datos_contacto.id_cliente)
+// 		INNER JOIN datos_comerciales ON (pedido.id_cliente=datos_comerciales.id_cliente)
+// 		INNER JOIN bodega ON (pedido.id_bodega=bodega.id_bodega)
+// 		INNER JOIN comuna ON (bodega.id_comuna=comuna.id_comuna)
+// 		INNER JOIN provincia ON (comuna.id_provincia=provincia.id_provincia)
+// 		INNER JOIN region ON (provincia.id_region=region.id_region) 
+// 		INNER JOIN cliente ON (pedido.id_cliente=cliente.id_cliente)
+// 		AND pedido.id_pedido= '.$id_pedido;
 		// . ' order by datos_contacto.id_datos_contacto desc limit 1';
 
-$datos_pedido = $conexion->mysqli->query($query)->fetch_object();
+// $datos_pedido = $conexion->mysqli->query($query)->fetch_object();
 
 // $html = "
 // <table border=1>
@@ -70,9 +70,10 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 
 if($conexion->mysqli->query("UPDATE pedido SET estado_pedido=3, estado_logistico=1 WHERE id_pedido=$id_pedido")) {
-    print_r(json_encode(array("success" => 1, "message" => "Su pago a crédito ha sido procesado exitosamente")));
+    echo (json_encode(array("success" => 1, "message" => "Su pago a crédito ha sido procesado exitosamente")));
 }
-else {print_r(json_encode(array("success" => 0, "message" => $conexion->mysqli->error)));
+else {
+	echo (json_encode(array("success" => 0, "message" => $conexion->mysqli->error)));
 }
 
 $conexion->desconectar();

b41defc8591d0e9d70489cdffa1cf0ab0f98cdb3
Eduardo Aguirrebeña <edu.aguirrebena@gmail.com>
2023-03-10 23:51:35 -0300

    test label

--- ws/pdf/index_prueba.php
+++ ws/pdf/index_prueba.php
@@ -95,27 +95,198 @@ $mpdf = new \Mpdf\Mpdf([
     'default_font' => 'roboto'
 ]);
 
-$plantilla = file_get_contents('etiqueta.html');
+// $plantilla = file_get_contents('etiqueta.html');
 
 
 foreach($bultos as $datos_bulto) {
+	// print_r($datos_bulto);
+	$html = '<!DOCTYPE html>
+	<html>
+	  <head>
+		<meta charset="utf-8" />
+		<title>Etiqueta</title>
+	  </head>
+	  <body>
+	
+	
+	
+		<div style="margin-bottom:10px">
+		  <table style="border-color: white !important;">
+			<tbody>
+			  <tr>
+				<td width="60%" class="text-center" style="border: 0px;">
+				  <p style="font-size:80px; color: #00a77f;">SPREAD</p>
+				</td>
+				<td width="40%" style="border: 0px;" class="text-left">
+				 www.spread.cl
+				<br>
+				
+				<br>
+				contacto@spread.cl
+				</td>
+			  </tr>
+			</tbody>
+		  </table>
+		</div>
+	
+	
+	
+		<table>
+		  <tbody>
+			<tr>
+			  <td width="50%" class="text-center"><b class="titulo-td">Comercio</b></td>
+			  <td width="50%" class="text-center"><b class="titulo-td">Bodega</b></td>
+			</tr>
+			<tr>
+			  <td width="50%" class="text-center">'.$datos_bulto["nombre_comercio"].'</b></td>
+			  <td width="50%" class="text-center">'.$datos_bulto["nombre_bodega_origen"].'</b></td>
+			</tr>
+			<tr>
+			  <td width="50%" class="text-center"><b class="titulo-td">Dirección</b></td>
+			  <td width="50%" class="text-center"><b class="titulo-td">Comuna</b></td>
+			</tr>
+			<tr>
+			  <td width="50%" class="text-center">'.$datos_bulto["direccion_origen"].'</b></td>
+			  <td width="50%" class="text-center">'.$datos_bulto["comuna_origen"].'</b></td>
+			</tr>
+			<tr>
+			  <td width="50%" class="text-center"><b class="titulo-td">Teléfono</b></td>
+			  <td width="50%" class="text-center"><b class="titulo-td">Nro Pedido </b></td>
+			</tr>
+			<tr>
+			  <td width="50%" class="text-center">'.$datos_bulto["telefono_comercio"].'</b></td>
+			  <td width="50%" class="text-center">'.$datos_bulto["pedido"].'</b></td>
+			</tr>
+		  </tbody>
+		</table>
+	
+	
+	
+		<div style="margin-bottom:70px; margin-top:70px"> 
+		  <table width="100%">
+			<tr>
+			  <td width="100%" style="border: 0px;">
+				<div class="codigo_barra" style="text-align: center;">
+				  <barcode code="'.$datos_bulto["track"].'" type="EAN128A" class="barcode" size="4" height="0.5"/>
+				</div>
+			  </td>
+			</tr>
+			<tr>
+			  <td align="center">
+				  <h1>'.$datos_bulto["track"].'</h1>
+				  <p>Numero de Guia</p>
+			  </td>
+			</tr>
+		  </table>
+		</div>
+	
+	
+	
+	
+	
+	 
+		<table>
+		  <thead>
+			<tr>
+			  <td class="text-center" colspan="2"><b class="titulo">Destinatario</b></td>
+			</tr>
+		  </thead>
+		  <tbody>
+			<tr>
+			  <td width="50%" class="text-center"><b class="titulo-td">Nombre</b></td>
+			  <td width="50%" class="text-center"><b class="titulo-td">Dirección</b></td>
+			</tr>
+			<tr>
+			  <td class="text-center">'.$datos_bulto["nombre_destinatario"].'</td>
+			  <td class="text-center">'.$datos_bulto["direccion_destinatario"].'</td>
+			</tr>
+			<tr>
+			  <td width="50%" class="text-center"><b class="titulo-td">Comuna</b></td>
+			  <td width="50%" class="text-center"><b class="titulo-td">Región</b></td>
+			</tr>
+			<tr>
+			  <td class="text-center">'.$datos_bulto["comuna_destinatario"].'</td>
+			  <td class="text-center">'.$datos_bulto["region_destinatario"].'</td>
+			</tr>
+			<tr>
+			  <td width="50%" class="text-center"><b class="titulo-td">Teléfono</b></td>
+			  <td width="50%" class="text-center"><b class="titulo-td">Email</b></td>
+			</tr>
+			<tr>
+			  <td class="text-center">'.$datos_bulto["telefono_destinatario"].'</td>
+			  <td class="text-center">'.$datos_bulto["email_destinatario"].'</td>
+			</tr>
+		  </tbody>
+		</table>
+	
+	
+	
+		<pagebreak></pagebreak>
+	  </body>
+	  <style>
+		body {
+		  font-size: 20px;
+		  min-height: 900px;
+		}
+		table {
+		  display: block;
+		  width: 100%;
+		  border-collapse: collapse;
+		  border-spacing: 2px;
+		  border-color: grey;
+		  margin-top: 5px;
+		}
+		td {
+		  border: 1px solid #e2e5e8;
+		  padding: 10px;
+		}
+		.td-carril {
+		  border: 3px solid black;
+		  padding: 0px;
+		}
+		.text-center {
+		  text-align: center !important;
+		}
+		.text-left {
+		  text-align: right !important;
+		  font-size: 30px;
+		}
+		.titulo-td {
+		  font-size: 20px !important;
+		}
+		.titulo-td-carril {
+		  font-size: 40px !important;
+		}
+		.titulo {
+		  font-size: 24px !important;
+		}
+		.codigo_barra {
+		  margin-bottom:40px;
+		  text-align:center;
+		  padding-right: 50px;
+		}
+		.carril {
+		  font-size: 100px;
+		}
+	  </style>
+	</html>';
 
-	$html = preg_replace_callback(
-		"|{(\w*)}|",
-		function ($matches) use ($datos_bulto) {
-			return $datos_bulto[$matches[1]];
-		},
-		$plantilla
-	);
+	// $html = preg_replace_callback(
+	// 	"|{(\w*)}|",
+	// 	function ($matches) use ($datos_bulto) {
+	// 		return $datos_bulto[$matches[1]];
+	// 	},
+	// 	$plantilla
+	// );
 	// echo $html;
 	$mpdf->WriteHTML($html);
 }
-if($debug) {
-	$mpdf->Output();
-}
-else {
+// if($debug) {
+// 	$mpdf->Output();
+// }
+// else {
 	$mpdf->Output("Etiquetas pedido ".$id_pedido.".pdf", 'D');
-}
+// }
 
 function upca($upc_code) {
     $upc = substr($upc_code,0,11);
